d2aff8f9f4e606e7df6360d5770f2e46c32a017e
Robin Lange <robin.langenc@gmail.com>
2022-10-11 22:39:34 +1100

    Updated predict.sh

--- predict.sh
+++ predict.sh
@@ -2,9 +2,8 @@
 
 python scripts/img2img.py \
     --seed 1 \
-    --prompt '' \
+    --prompt "$2" \
     --outdir 'outputs/generated_gundam' \
     --n_samples 1 \
     --config 'configs/stable-diffusion/gundam.yaml' \
-    --ckpt "$1" \
-    --init-img "$2"
+    --ckpt "$1"
